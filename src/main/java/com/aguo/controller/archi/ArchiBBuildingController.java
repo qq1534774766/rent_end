fcc9f2e077d6c889dcb2ee68ccd9ffa69e41f28e
wenqiaogang <aguowqg@qq.com>
2023-03-19 00:18:54 +0800

    去除除以0的bug

--- src/main/java/com/aguo/controller/archi/ArchiBBuildingController.java
+++ src/main/java/com/aguo/controller/archi/ArchiBBuildingController.java
@@ -2,12 +2,20 @@ package com.aguo.controller.archi;
 
 import com.aguo.entity.ArchiBBuilding;
 import com.aguo.service.ArchiBBuildingService;
+import com.aguo.vo.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+/**
+ * 楼盘管理
+ *
+ * @author Administrator
+ */
 @RestController
 @RequestMapping("/manage")
 public class ArchiBBuildingController {
@@ -15,7 +23,7 @@ public class ArchiBBuildingController {
     private ArchiBBuildingService archiBBuildingService;
 
     @RequestMapping("/listBuilding")
-    public List<ArchiBBuilding> listBuilding(){
+    public List<ArchiBBuilding> listBuilding() {
         return archiBBuildingService.list();
     }
     @RequestMapping("/addBuilding")
